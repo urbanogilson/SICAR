4ecc521e5e9e18229ca3e47ecc0b72e900ccb2cf
Gilson Urbano <guf@hotmail.com.br>
2021-08-08 18:37:23 +0200

    Init new version

---
+++ src/captcha.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import matplotlib.image as mpimg
+import re
+import pytesseract
+from pathlib import Path
+from cv2 import cv2
+import numpy as np
+from exceptions import NotImplemented
+
+
+class Captcha(object):
+    def _png_to_jpg(self, captcha: Path = Path("temp/captcha.png")) -> Path:
+        captcha_jpg = captcha.with_suffix(".png")
+
+        mpimg.imsave(
+            captcha_jpg,
+            mpimg.imread(captcha, 0),
+            cmap="gray",
+            vmin=0,
+            vmax=255,
+        )
+
+        return captcha_jpg
+
+    def _get_captcha(self, captcha: Path = Path("temp/captcha.png")) -> str:
+        raise NotImplemented()
+
+
+class Tesseract(Captcha):
+    __custom_l_psm_config = r"-l eng --psm 7 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+    def _get_captcha(self, captcha: Path = Path("temp/captcha.png")) -> str:
+        return re.sub(
+            "[^A-Za-z0-9]+",
+            "",
+            pytesseract.image_to_string(
+                self._process_captcha(captcha),
+                config=self.__custom_l_psm_config,
+            ),
+        )
+
+    def _improve_image(self, image):
+        _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU + 2)
+        image = cv2.dilate(image, np.ones((3, 2), np.uint8), iterations=1)
+        image = cv2.erode(image, np.ones((4, 1), np.uint8), iterations=2)
+        image = cv2.dilate(image, np.ones((3, 1), np.uint8), iterations=2)
+        image = cv2.erode(image, np.ones((2, 1), np.uint8), iterations=2)
+        return image
+
+    def _process_captcha(self, captcha: Path = Path("temp/captcha.png")):
+
+        captcha_jpg = self._png_to_jpg(captcha)
+
+        img = cv2.cvtColor(cv2.imread(captcha_jpg, -1), cv2.COLOR_BGR2GRAY)
+
+        res = self.__improve_image(img)
+
+        mpimg.imsave(captcha_jpg, res, cmap="gray")
+
+        return captcha_jpg
---
+++ src/exceptions.py
@@ -0,0 +1,66 @@
+class NotImplemented(Exception):
+    """Exception raised when the method is not implemented."""
+
+    pass
+
+
+class EmailNotValidException(Exception):
+    """Exception raised for errors in the input email.
+
+    Attributes:
+        email -- email address which caused the error
+    """
+
+    def __init__(self, email: str):
+        self.email = email
+        super().__init__("Email {} not valid!".format(self.email))
+
+    pass
+
+
+class UrlNotOkException(Exception):
+    """Exception raised when the input url is not ok.
+
+    Attributes:
+        url -- url address which caused the error
+    """
+
+    def __init__(self, url: str):
+        self.url = url
+        super().__init__("Oh no! Failed to access {}!".format(self.url))
+
+    pass
+
+
+class StateCodeNotValidException(Exception):
+    """Exception raised when the input state is not a brazian state code.
+
+    Attributes:
+        state -- state code which caused the error
+    """
+
+    def __init__(self, state: str):
+        self.state = state
+        super().__init__("State code {} not valid!".format(self.state))
+
+    pass
+
+
+class FailedToDownloadCaptchaException(Exception):
+    """Exception raised when the captcha download failed."""
+
+    def __init__(self, message: str = "Failed to download captcha!"):
+        self.message = message
+        super().__init__(self.message)
+
+    pass
+
+
+class FailedToDownloadShapefileException(Exception):
+    """Exception raised when the shapefile download failed."""
+
+    def __init__(self, message: str = "Failed to download shapefile!!"):
+        self.message = message
+        super().__init__(self.message)
+
+    pass
