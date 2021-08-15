f2291388f4c28ef2a4afe310055ffbc80c2b7ae7
Gilson Urbano <guf@hotmail.com.br>
2021-08-15 17:47:26 +0200

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="SICAR",
-    version="0.1.0",
+    version="0.2.0",
     author="Gilson Urbano",
     author_email="gilson@gilsonurbano.com",
     packages=["SICAR"],
