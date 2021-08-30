f2e7031b52cc84412ca06dc8559340417fdc55bc
Gilson Urbano <guf@hotmail.com.br>
2021-08-30 16:44:23 +0200

    Downgrade urllib3 and opencv

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     long_description=open("README.md").read(),
     install_requires=[
         "requests>=2.22.0",
-        "urllib3>=1.25.8",
+        "urllib3>=1.24.3",
         "pytesseract>=0.3.7",
-        "opencv-python>=4.5.1.48",
+        "opencv-python>=4.1.2.30",
         "numpy>=1.19.5",
         "tqdm>=4.56.2",
         "matplotlib>=3.2.2",
