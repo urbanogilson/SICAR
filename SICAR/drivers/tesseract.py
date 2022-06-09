5f613ab89462174575a9581885c6a2d09efd8f88
Gilson Urbano <guf@hotmail.com.br>
2022-06-10 00:02:20 +0200

    Closes #7; Fix cv2 import

--- SICAR/drivers/tesseract.py
+++ SICAR/drivers/tesseract.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import matplotlib.image as mpimg
 import re
 import pytesseract
-from cv2 import cv2
+import cv2
 import numpy as np
 
 from SICAR.drivers.captcha import Captcha
