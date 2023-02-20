e977084d0e178ba968b2882e94e7bbb9608146eb
Gilson Urbano <guf@hotmail.com.br>
2023-02-20 20:05:44 +0000

    Add csv download test

--- SICAR/tests/sicar.py
+++ SICAR/tests/sicar.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import unittest
-from SICAR import Sicar
+from SICAR import Sicar, OutputFormat
 
 
 class TestSicarBase(unittest.TestCase):
@@ -11,8 +11,17 @@ class TestSicarBase(unittest.TestCase):
     def test_get_base_url(self):
         self.assertRegex(self._car.get_base_url(), "car.gov.br")
 
-    def test_download_city_by_code(self):
-        self.assertIsInstance(self._car.download_city_code("3120870"), Path)
+    def test_download_city_by_code_shapefile(self):
+        self.assertIsInstance(
+            self._car.download_city_code("3120870"),
+            Path,
+        )
+
+    def test_download_city_by_code_csv(self):
+        self.assertIsInstance(
+            self._car.download_city_code("3120870", output_format=OutputFormat.CSV),
+            Path,
+        )
 
 
 if __name__ == "__main__":
