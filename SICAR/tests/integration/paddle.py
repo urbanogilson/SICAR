1ebb23eb02e66c5dbeb5c1893ff93bd7c17726d5
Gilson Urbano <guf@hotmail.com.br>
2023-06-27 18:45:54 +0000

    Fix integration tests

--- SICAR/tests/integration/paddle.py
+++ SICAR/tests/integration/paddle.py
@@ -1,21 +1,21 @@
 import unittest
 from SICAR.drivers import Paddle
 from pathlib import Path
+from PIL import Image
 
 
 class TestPaddleDriver(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self._driver = Paddle()
-        self._captchas = list(Path("SICAR/tests/captchas").glob("*.png"))
+        self._captchas = list(Path("SICAR/tests/integration/captchas").glob("*.png"))
 
-    def test_get_captcha(self):
+    def test_get_captchas(self):
         for captcha in self._captchas:
-            self.assertEqual(
-                self._driver._get_captcha(captcha),
-                captcha.stem,
-            )
-
-
-if __name__ == "__main__":
-    unittest.main()
+            with self.subTest(
+                msg="Checking paddle captcha detection", captcha_name=captcha.stem
+            ):
+                self.assertEqual(
+                    self._driver.get_captcha(Image.open(captcha)),
+                    captcha.stem,
+                )
--- SICAR/tests/integration/tesseract.py
+++ SICAR/tests/integration/tesseract.py
@@ -1,22 +1,33 @@
 import unittest
 from SICAR.drivers import Tesseract
 from pathlib import Path
+from PIL import Image
+import os
 
 
 class TestTesseractDriver(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self._driver = Tesseract()
-        self._captcha = "Ca7Qk"
+        self._captchas = Path("SICAR/tests/integration/captchas")
 
-    def test_get_captcha(self):
+    def test_get_captcha_AbgBy(self):
+        captcha = Path(os.path.join(self._captchas, "AbgBy")).with_suffix(".png")
         self.assertEqual(
-            self._driver._get_captcha(
-                Path(f"SICAR/tests/captchas/{self._captcha}.png")
-            ),
-            self._captcha,
+            self._driver.get_captcha(Image.open(captcha)),
+            captcha.stem,
         )
 
+    def test_get_captcha_Ca7Qk(self):
+        captcha = Path(os.path.join(self._captchas, "Ca7Qk")).with_suffix(".png")
+        self.assertEqual(
+            self._driver.get_captcha(Image.open(captcha)),
+            captcha.stem,
+        )
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_get_captcha_ZS7pc(self):
+        captcha = Path(os.path.join(self._captchas, "ZS7pc")).with_suffix(".png")
+        self.assertEqual(
+            self._driver.get_captcha(Image.open(captcha)),
+            captcha.stem,
+        )
