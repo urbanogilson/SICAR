1e9f97e47d35a53835a1ea819ea0792e177c5a66
Gilson Urbano <guf@hotmail.com.br>
2024-03-23 18:52:47 +0000

    Fix UT

--- SICAR/tests/unit/sicar.py
+++ SICAR/tests/unit/sicar.py
@@ -70,6 +70,7 @@ class SicarTestCase(unittest.TestCase):
             {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
                 "Accept-Encoding": "gzip, deflate, br",
+                "Connection": "close",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             }
         )
