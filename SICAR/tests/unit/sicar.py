b61859a04516ac6e9ecd0c9240196341d11dbf0b
Cainã <51023074+rupestre-campos@users.noreply.github.com>
2024-08-02 16:00:11 -0300

    Update SICAR URL (#30)
    * Update SICAR URL 
    * Limit open cv, and numpy versions

--- SICAR/tests/unit/sicar.py
+++ SICAR/tests/unit/sicar.py
@@ -41,7 +41,9 @@ class SicarTestCase(unittest.TestCase):
     def test_initialize_cookies(self, mock_get):
         self.mock_initialize_cookies.stop()
         Sicar(driver=self.mocked_captcha)
-        mock_get.assert_called_once_with("https://www.car.gov.br/publico/imoveis/index")
+        mock_get.assert_called_once_with(
+            "https://consultapublica.car.gov.br/publico/imoveis/index"
+        )
         self.mock_initialize_cookies.start()
 
     def test_ocr_driver_integration(self):
@@ -103,7 +105,7 @@ class SicarTestCase(unittest.TestCase):
         captcha_image = sicar._download_captcha()
 
         sicar._get.assert_called_once_with(
-            f"https://www.car.gov.br/publico/municipios/ReCaptcha?id={int(random.random() * 1000000)}"
+            f"https://consultapublica.car.gov.br/publico/municipios/ReCaptcha?id={int(random.random() * 1000000)}"
         )
         Image.open.assert_called_once()
         self.assertEqual(captcha_image, mock_image)
@@ -160,7 +162,7 @@ class SicarTestCase(unittest.TestCase):
 
         stream_mock.assert_called_once_with(
             "GET",
-            r"https://www.car.gov.br/publico/estados/downloadBase?idEstado=MG&tipoBase=APPS&ReCaptcha=abc123",
+            r"https://consultapublica.car.gov.br/publico/estados/downloadBase?idEstado=MG&tipoBase=APPS&ReCaptcha=abc123",
         )
         mock_path.assert_called_once_with(f"{folder}/{state}_{polygon.value}")
         mock_open.assert_called_once_with(
