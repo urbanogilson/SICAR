54bfbf7ddd57e12929101f59c30d83a2e50a5625
Cainã K. Campos <rupestre.campos@gmail.com>
2024-08-02 15:32:07 -0300

    fix tests failing and formatting

--- SICAR/tests/unit/polygon.py
+++ SICAR/tests/unit/polygon.py
@@ -7,14 +7,18 @@ class UrlTestCase(unittest.TestCase):
         self.assertEqual(Url._BASE, "https://consultapublica.car.gov.br/publico")
 
     def test_index_url(self):
-        self.assertEqual(Url._INDEX, "https://consultapublica.car.gov.br/publico/imoveis/index")
+        self.assertEqual(
+            Url._INDEX, "https://consultapublica.car.gov.br/publico/imoveis/index"
+        )
 
     def test_downloads_url(self):
         self.assertEqual(
-            Url._DOWNLOAD_BASE, "https://consultapublica.car.gov.br/publico/estados/downloadBase"
+            Url._DOWNLOAD_BASE,
+            "https://consultapublica.car.gov.br/publico/estados/downloadBase",
         )
 
     def test_captcha_url(self):
         self.assertEqual(
-            Url._RECAPTCHA, "https://consultapublica.car.gov.br/publico/municipios/ReCaptcha"
+            Url._RECAPTCHA,
+            "https://consultapublica.car.gov.br/publico/municipios/ReCaptcha",
         )
