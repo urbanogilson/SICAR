f9babf3b1572ef74f42a653631c274edc2c7c057
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2025-06-30 20:09:38 -0300

    Add integration test for download resume (#3)

--- SICAR/tests/integration/sicar.py
+++ SICAR/tests/integration/sicar.py
@@ -1,5 +1,8 @@
+import os
 from pathlib import Path
 import unittest
+from unittest.mock import patch
+
 from SICAR import Sicar, State, Polygon
 
 
@@ -13,6 +16,39 @@ class TestSicarBase(unittest.TestCase):
             self._car.download_state(State.RR, Polygon.AREA_FALL, debug=True),
             Path,
         )
+
+    @patch("SICAR.sicar.Sicar._download_polygon")
+    def test_download_resume(self, mock_download):
+        state = State.RR
+        polygon = Polygon.AREA_FALL
+        folder = "temp"
+        path = Path(folder) / f"{state.value}_{polygon.value}.zip"
+
+        if path.exists():
+            os.remove(path)
+
+        def mock_download_side_effect(*args, **kwargs):
+            if not path.exists():
+                with open(path, "wb") as fd:
+                    fd.write(b"Partial content")
+            else:
+                with open(path, "ab") as fd:
+                    fd.write(b" - resumed content")
+
+            return path
+
+        mock_download.side_effect = mock_download_side_effect
+
+        self._car.download_state(state, polygon, folder=folder, debug=True)
+        downloaded_path = self._car.download_state(state, polygon, folder=folder, debug=True)
+
+        self.assertTrue(downloaded_path.exists())
+
+        with open(downloaded_path, "rb") as fd:
+            content = fd.read()
+
+        self.assertIn(b"Partial content", content)
+        self.assertIn(b"resumed content", content)
 
     def test_get_release_dates(self):
         self.assertIsInstance(self._car.get_release_dates(), dict)
