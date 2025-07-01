12649e4f0660cd8efdc0e0e0f2d9b7704f80fd8d
Malnati <ricardomalnati@gmail.com>
2025-07-01 15:54:46 -0300

    rebasing

--- examples/download_state.py
+++ examples/download_state.py
@@ -45,12 +45,25 @@ car.download_state(
 from SICAR import Sicar, Polygon, State
 from SICAR.drivers import Paddle, Tesseract
 
+parser = argparse.ArgumentParser(description="Download SICAR state polygon.")
+parser.add_argument("--state", type=str, default=os.getenv("STATE", "DF"))
+parser.add_argument("--polygon", type=str, default=os.getenv("POLYGON", "APPS"))
+parser.add_argument("--folder", type=str, default=os.getenv("FOLDER", "data/DF"))
+parser.add_argument("--tries", type=int, default=int(os.getenv("TRIES", "25")))
+parser.add_argument("--debug", type=lambda x: str(x).lower() == "true", default=os.getenv("DEBUG", "False"))
+parser.add_argument("--timeout", type=int, default=int(os.getenv("TIMEOUT", "30")))
+parser.add_argument("--max_retries", type=int, default=int(os.getenv("MAX_RETRIES", "5")))
+args = parser.parse_args()
+
 # Read parameters from environment variables with reasonable defaults
-state = State[os.getenv("STATE", "DF")]
-polygon = Polygon[os.getenv("POLYGON", "APPS")]
-folder = os.getenv("FOLDER", "data/DF")
-debug = os.getenv("DEBUG", "False").lower() == "true"
-tries = int(os.getenv("TRIES", "25"))
+state = State[args.state] if args.state in State.__members__ else State[os.getenv("STATE", "DF")]
+polygon = Polygon[args.polygon] if args.polygon in Polygon.__members__ else Polygon[os.getenv("POLYGON", "APPS")]
+folder = args.folder
+tries = args.tries
+debug = args.debug
+chunk_size = 1024
+timeout = args.timeout
+max_retries = args.max_retries
 
 # Create Sicar instance (default driver is Tesseract)
 car = Sicar(driver=Tesseract)
